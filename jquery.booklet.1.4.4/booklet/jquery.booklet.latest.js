1160247a1e26d77926dc5f39e1f188d2b8ac4f3c
Giannipunt <gianniandroid43@gmail.com>
2014-08-27 13:36:45 +0200

    files

--- jquery.booklet.1.4.4/booklet/jquery.booklet.latest.js
+++ jquery.booklet.1.4.4/booklet/jquery.booklet.latest.js
@@ -2075,8 +2075,8 @@
     // define default options
     $.fn.booklet.defaults = {
         name: null,                            // name of the booklet to display in the document title bar
-        width: 800,                             // container width
-        height: 600,                             // container height
+        width: 500,                             // container width
+        height: 400,                             // container height
         speed: 1000,                            // speed of the transition between pages
         direction: 'LTR',                           // direction of the overall content organization, default LTR, left to right, can be RTL for languages which read right to left
         startingPage: 0,                               // index of the first page to be displayed
